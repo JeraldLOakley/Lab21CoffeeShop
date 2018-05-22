e16f8874e18478126f83125c1917cffd653e564d
JeraldLOakley <Jerryoakley989@gmail.com>
2018-05-22 08:48:34 -0400

    first commit

---
+++ Lab21/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab21.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        public ActionResult Submission(string fName, string lName, string email, long phone = 0)
+        {
+            ViewBag.RegFirstName = fName;
+            ViewBag.RegLastName = lName;
+            ViewBag.RegEmail = email;
+            ViewBag.RegPhone = phone;
+            return View();
+        }
+
+
+
+
+
+    }
+
+}
